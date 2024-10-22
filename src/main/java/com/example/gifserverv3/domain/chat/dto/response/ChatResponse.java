6ebae1a9d85dd7ce23bc063ae500f3190a61dba1
kimjihoon3106 <s24021@gsm.hs.kr>
2024-10-22 22:59:38 +0900

    feat : dto about chat

---
+++ src/main/java/com/example/gifserverv3/domain/chat/dto/response/ChatResponse.java
@@ -0,0 +1,21 @@
+package com.example.gifserverv3.domain.chat.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatResponse {
+    private Long id;
+    private Sender sender;
+    private String message;
+    private LocalDateTime sendDate;
+
+}
