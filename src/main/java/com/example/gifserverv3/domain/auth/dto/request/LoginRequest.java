70cdfacc330b8f57e8f3ba2c987551838d3250aa
kimjihoon3106 <s24021@gsm.hs.kr>
2024-10-22 23:01:56 +0900

    feat : dto about request LoginRequest for login

---
+++ src/main/java/com/example/gifserverv3/domain/auth/dto/request/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.gifserverv3.domain.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String password;
+}
