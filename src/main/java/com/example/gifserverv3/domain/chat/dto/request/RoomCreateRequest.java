0f4bcf08e37e86ed230fbacdd5e03db6e611fac1
kimjihoon3106 <s24021@gsm.hs.kr>
2024-10-22 23:02:28 +0900

    feat : dto roomcreate request and response

---
+++ src/main/java/com/example/gifserverv3/domain/chat/dto/request/RoomCreateRequest.java
@@ -0,0 +1,10 @@
+package com.example.gifserverv3.domain.chat.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RoomCreateRequest {
+    private Long requestId;
+}
