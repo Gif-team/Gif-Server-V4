31432c130de1188d6c2199f8a905855b65a01de8
kimjihoon3106 <s24021@gsm.hs.kr>
2024-10-22 22:50:46 +0900

    init

---
+++ src/main/java/com/example/gifserverv3/GifServerV3Application.java
@@ -0,0 +1,13 @@
+package com.example.gifserverv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifServerV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifServerV3Application.class, args);
+    }
+
+}
