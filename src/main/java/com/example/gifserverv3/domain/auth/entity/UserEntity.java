ff4a9520b0b9dde0b446c457b76513cc3d45a1ac
kimjihoon3106 <s24021@gsm.hs.kr>
2024-10-22 22:55:22 +0900

    feat : entity userentity

---
+++ src/main/java/com/example/gifserverv3/domain/auth/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package com.example.gifserverv3.domain.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "users_id")
+    private Long usersId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+}
