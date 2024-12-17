2aad0a85c019c9d75647485a2340f56b198e5c07
kimjihoon3106 <s24021@gsm.hs.kr>
2024-12-17 14:33:24 +0900

    feat : postrepository to use jparepository postentity

---
+++ src/main/java/com/example/gifserverv3/domain/post/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.gifserverv3.domain.post.repository;
+
+import com.example.gifserverv3.domain.post.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+}
