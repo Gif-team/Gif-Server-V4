8fd42d389ea8acba549db99b2cc26302a5e07c02
kimjihoon3106 <s24021@gsm.hs.kr>
2024-12-28 23:04:38 +0900

    feat : getpostbysession

--- src/main/java/com/example/gifserverv3/domain/post/repository/PostRepository.java
+++ src/main/java/com/example/gifserverv3/domain/post/repository/PostRepository.java
@@ -2,11 +2,15 @@ package com.example.gifserverv3.domain.post.repository;
 
 import com.example.gifserverv3.domain.post.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+    @Query("SELECT p FROM PostEntity p WHERE p.writerId = :userId")
+    List<PostEntity> getPostsBySession(@Param("userId") Long userId);
+
     @Query("SELECT COUNT(p) FROM PostEntity p WHERE p.writerId = :userId AND p.category = true")
     long countTrueCategoryPostsByUserId(@Param("userId") Long userId);
 
