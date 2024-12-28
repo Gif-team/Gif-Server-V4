d10a14545efb1ecad8615419e3f4b67676f2e560
kimjihoon3106 <s24021@gsm.hs.kr>
2024-12-28 23:06:26 +0900

    feat : 3badge

--- src/main/java/com/example/gifserverv3/domain/badge/controller/BadgeController.java
+++ src/main/java/com/example/gifserverv3/domain/badge/controller/BadgeController.java
@@ -1,9 +1,7 @@
 package com.example.gifserverv3.domain.badge.controller;
 
-import com.example.gifserverv3.domain.auth.entity.UserEntity;
 import com.example.gifserverv3.domain.badge.service.BadgeServiceImpl;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
