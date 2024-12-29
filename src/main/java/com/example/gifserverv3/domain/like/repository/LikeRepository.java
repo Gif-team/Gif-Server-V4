ac47ac38b016e9dd1d6136ab6bdde031e56e2586
kimjihoon3106 <s24021@gsm.hs.kr>
2024-12-29 18:50:34 +0900

    feat : deletedByPostId method

--- src/main/java/com/example/gifserverv3/domain/like/repository/LikeRepository.java
+++ src/main/java/com/example/gifserverv3/domain/like/repository/LikeRepository.java
@@ -4,14 +4,21 @@ import com.example.gifserverv3.domain.auth.entity.UserEntity;
 import com.example.gifserverv3.domain.like.entity.LikeEntity;
 import com.example.gifserverv3.domain.post.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
 
-    // 특정 user와 post에 대한 LikeEntity 조회
     Optional<LikeEntity> findByUserAndPost(UserEntity user, PostEntity post);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM LikeEntity l WHERE l.post.postid = :postId")
+    void deleteByPostId(@Param("postId") Long postId);
 }
